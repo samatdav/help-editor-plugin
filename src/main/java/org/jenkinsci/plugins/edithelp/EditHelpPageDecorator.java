d0956a64dbac094e846563933657932900b0ef4d
Sam <samatdav@gmail.com>
2016-08-07 22:38:46 +0300

    cleanup

--- src/main/java/org/jenkinsci/plugins/edithelp/EditHelpPageDecorator.java
+++ src/main/java/org/jenkinsci/plugins/edithelp/EditHelpPageDecorator.java
@@ -2,14 +2,7 @@ package org.jenkinsci.plugins.edithelp;
 
 import hudson.Extension;
 import hudson.model.PageDecorator;
-import net.sf.json.JSONObject;
-
-import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public class EditHelpPageDecorator extends PageDecorator {
-    public EditHelpPageDecorator() {
-        super(EditHelpPageDecorator.class);
-        load();
-    }
 }
