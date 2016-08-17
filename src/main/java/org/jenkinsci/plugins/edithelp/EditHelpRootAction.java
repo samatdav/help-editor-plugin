14f29592839d5675c0a3a2adf08c527b31cf010b
Sam <samatdav@gmail.com>
2016-08-17 11:48:16 +0300

    fixed bugs

--- src/main/java/org/jenkinsci/plugins/edithelp/EditHelpRootAction.java
+++ src/main/java/org/jenkinsci/plugins/edithelp/EditHelpRootAction.java
@@ -11,12 +11,12 @@ import hudson.cli.PrivateKeyProvider;
 import hudson.model.RootAction;
 import jenkins.model.Jenkins;
 import org.apache.commons.lang.StringUtils;
-import org.kohsuke.accmod.Restricted;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.Stapler;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.logging.Logger;
 
 import static java.nio.file.Files.readAllLines;
@@ -27,39 +27,33 @@ public class EditHelpRootAction implements RootAction {
 
     private static final Logger LOGGER = Logger.getLogger(PrivateKeyProvider.class.getName());
 
-    //array for cache storage
-    private Map<String, String> arrayOfHelp = new TreeMap<String, String>();
+    //hashMap for cache storage
+    private transient Map<String, String> hashMapOfHelp = new HashMap<String, String>();
 
     //reading files into cache
     public EditHelpRootAction() throws IOException, FileNotFoundException {
-        Jenkins jn = Jenkins.getActiveInstance();
-        if (jn != null) {
-            File dirfile = jn.getRootDir();         
-            if(dirfile != null) {
-                //check if the directory exists
-                File helpManagerFile = new File(dirfile, "helpmanager");
+        File dirfile = Jenkins.getActiveInstance().getRootDir();
+        if(dirfile != null) {
 
-                if(!helpManagerFile.exists())
-                    if (!helpManagerFile.mkdirs()) {
-                        LOGGER.log(FINE,"unable create directory");
-                    }
+            //check if the directory exists
+            File helpManagerFile = new File(dirfile, "helpmanager");
 
-                //reading files from the directory
-                if (helpManagerFile.getAbsolutePath() != null) {
-                    File dir = new File(helpManagerFile.getAbsolutePath());
-                    File[] files = dir.listFiles();
-                    if (files != null){
-                        for (File file : files) {
-                            if (file.isFile()) {
-                                List<String> getStringList = readAllLines(file.toPath(),StandardCharsets.UTF_8);
-                                String getListtoStr = StringUtils.join(getStringList, "\n");
-                                arrayOfHelp.put(file.getName(),getListtoStr);
-                            }
-                        }
-                    }
+            if(!helpManagerFile.exists())
+                if (!helpManagerFile.mkdirs()) {
+                    LOGGER.log(FINE,"Unable create directory: "+helpManagerFile.getAbsolutePath());
                 }
-            }            
-        }            
+
+            //reading files from the directory
+            if (helpManagerFile.getAbsolutePath() != null) {
+                File dir = new File(helpManagerFile.getAbsolutePath());
+                File[] files = dir.listFiles();
+                if (files != null)
+                    for (File file : files)
+                        if (file.isFile())
+                            hashMapOfHelp.put(file.getName(),readFromFile(file));
+            }
+        }
+
     }
 
     @Override
@@ -77,57 +71,70 @@ public class EditHelpRootAction implements RootAction {
         return "helpmanager";
     }
 
-    //copying from array
+    //copying from HashMap
     public String getHelpInfo() {
         //process get request
         String className = Stapler.getCurrentRequest().getParameter("class");
-        return arrayOfHelp.get(className+".html");
+        String Help = hashMapOfHelp.get(className+".html");
+        if(Help != null)
+            return Help;
+        else
+            return "";
+    }
+
+    private void writeInFile(File file, String info){
+        try(BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8))){
+            out.write(info);
+            out.flush();
+            out.close();
+        } catch (IOException exname){
+            LOGGER.log(FINE,"file was not written:"+file.getAbsolutePath());
+        }
+    }
+
+    private String readFromFile(File file) throws IOException{
+        try{
+            List<String> getStringList = readAllLines(file.toPath(),StandardCharsets.UTF_8);
+            return StringUtils.join(getStringList, "\n");
+
+        } catch (IOException exname){
+            LOGGER.log(FINE,"help file was not read:"+file.getAbsolutePath());
+            return "";
+        }
     }
 
     //save text area into file and array
-    public void doUpdateHelpInfo() {
+    public void doUpdateHelpInfo(@QueryParameter("class") String className, @QueryParameter("textArea") String updatedClassText) {
+        
         //process get request
-        String className = Stapler.getCurrentRequest().getParameter("class");
-        String updatedClassText = Stapler.getCurrentRequest().getParameter("textArea");
-        //replacement or creation of the string
-        arrayOfHelp.put(className+".html",updatedClassText);
-
-        //writing into a file
         if (className != null) {
-            Jenkins jn = Jenkins.getActiveInstance();
-            if (jn != null) {
-                File dirfile = jn.getRootDir();         
-                String dirName = null;         
-                if(dirfile != null) {
-                    //check if the directory exists
-                    File helpManagerFile = new File(dirfile, "helpmanager");
-                    if(!helpManagerFile.exists())
-                        if (!helpManagerFile.mkdirs()) {
-                            LOGGER.log(FINE,"unable create directory");
-                        }
+            //replacement or creation of the string
+            hashMapOfHelp.put(className+".html",updatedClassText);
 
-                    File newFile = new File(helpManagerFile.getAbsolutePath() + "/" + className + ".html");
-                    //check if the file exists
-                    if (!newFile.exists()) {
-                        try{
-                            if (!newFile.createNewFile()) {
-                                LOGGER.log(FINE,"help file was not created");
-                            }
-                        } catch (IOException exname){
-                            LOGGER.log(FINE,"help file was not created");
-                        }
-                    }
+            //writing into a file
+            File dirfile = Jenkins.getActiveInstance().getRootDir();
+            if(dirfile != null) {
 
-                    //stream writing into file
-                    try(BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFile), StandardCharsets.UTF_8))){
-                        out.write(updatedClassText);
-                        out.flush();
-                        out.close();
+                //check if the directory exists
+                File helpManagerFile = new File(dirfile, "helpmanager");
+                if(!helpManagerFile.exists())
+                    if (!helpManagerFile.mkdirs())
+                        LOGGER.log(FINE,"unable create helpmanager directory");
+
+                File newFile = new File(helpManagerFile.getAbsolutePath() + "/" + className + ".html");
+                //check if the file exists
+                if (!newFile.exists()) {
+                    try{
+                        if (!newFile.createNewFile())
+                            LOGGER.log(FINE,"help file was not created:"+newFile.getAbsolutePath());
                     } catch (IOException exname){
-                        LOGGER.log(FINE,"file was not written");
+                        LOGGER.log(FINE,"help file was not created:"+newFile.getAbsolutePath());
                     }
                 }
+
+                writeInFile(newFile, updatedClassText);
             }
-        }
+
+        } 
     }
 }
